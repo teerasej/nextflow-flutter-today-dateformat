1da0090cc83ffaf2fee3485bd1a298544caddfd3
Teerasej <teerasej@gmail.com>
2020-06-06 20:04:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/nextflow_dateformat_thai/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nextflow_dateformat_thai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+
+  // Intl.defaultLocale = "th";
+  initializeDateFormatting();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nextflow Flutter Widget Today',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter 3 นาที: เสกวันเดือนปี'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  DateTime myDateTime = DateTime.now();
+
+
+  @override
+  Widget build(BuildContext context) {
+   
+    // var formatter = DateFormat.y();
+    // var formatter = DateFormat.yM();
+    // var formatter = DateFormat.yMMM();
+    // var formatter = DateFormat.yMMMM();
+    // var formatter = DateFormat.yMMMMd();
+    var formatter = DateFormat.yMMMMEEEEd('zh');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Wrap(
+          direction: Axis.vertical,
+          spacing: 20,
+          children: <Widget>[
+            Text(
+              '${formatter.format(myDateTime)}',
+              style: TextStyle(fontSize: 24)
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
